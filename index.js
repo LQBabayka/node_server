ac30b77a118d5e92e7aa4546bb3174879df86b7a
Alex Tim <timashevalexandr@gmail.com>
2021-08-04 23:10:58 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+let http = require('http'); //Подключил модуль для работы http
+
+console.log('start');
+http.createServer(function (request, response) {
+    if (request.url != '/favicon.ico') {
+
+
+        console.log(request.url); //Выводится: /(та часть, которая идет после локалхоста и порта) и /favicon.ico
+        // console.log(request.method); //Выводится: GET
+        // console.log(request.headers); //Выводится: host, connection, pragma, accept,  referer (С подробностями)
+
+            //Тут самая основная часть, без которой ничего не работает
+        // response.writeHead(200, {'Content-Type': 'text/html'});// в этом случае html теги работают, если заменить на plain - будет только текст
+        // response.write('<h1>What guys, anime?</h1>');
+        // response.end();
+        let dveste = response.writeHead(200, { 'Content-Type': 'text/html' });// в этом случае html теги работают, если заменить на plain - будет только текст
+        let urla = request.url
+        switch (urla) {
+            case '/index':
+                dveste
+                response.write('<h1>Tuta index</h1>');
+                break;
+
+            case '/about':
+                dveste
+                response.write('<h1>Tuta abaut</h1>');
+                break;
+
+            case '/epta':
+                dveste
+                response.write('<h1>Nu a tut epta</h1>');
+                break;
+                
+            default: //этат ема нужна, если ни одно значение не подойдет
+            response.writeHead(404, { 'Content-Type': 'text/html' });
+            response.write('404');
+        }
+
+        response.end();
+    }
+}).listen(8888);
+
+//Запуск скрипта через команду: node index.js В браузере страница: http://localhost:8888/
